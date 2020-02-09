7da0109b0531b0cb67ed2a8a0529d83f6238cbdc
Leandro da Silva Alves <leandro.alves@invillia.com>
2020-02-09 20:08:51 -0300

    Atualizar Titulo

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,6 @@ import Tabuleiro from './componentes/tabuleiro';
 
 export default props =>
   <div>
-    <h1>Jogo da Velha</h1>
+    <h1>Jogo da Velha <small>com React</small></h1>
     <Tabuleiro title="Tabuleiro" />
   </div>
