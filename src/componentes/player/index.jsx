ad4a406076c2f319e92f049be80e0d95c4b715b4
Leandro da Silva Alves <leandro.alves@invillia.com>
2020-02-09 09:25:58 -0300

    Definindo vencedor

---
+++ src/componentes/player/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default props =>
+  <div>
+    <div>{props.name} : 00</div>
+  </div>
--- src/componentes/tabuleiro/index.jsx
+++ src/componentes/tabuleiro/index.jsx
@@ -6,21 +6,46 @@ import Button from '../button';
 const initialState = {
   values: Array(9).fill(null),
   count: 0,
+  vencedor: false,
 };
 
 export default class Tabuleiro extends Component {
 
   state = {...initialState};
+  formasDeGanho = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6],
+    ];
 
   clickButton(indice) {  
     
-    if(this.state.count === 9 || this.state.values[indice])
+    if(this.state.count === 9 || this.state.values[indice] || this.state.vencedor)
       return;
 
     let newState = {...this.state};
     newState.values[indice] = this.state.count % 2 === 0 ? 'X' : 'O';
     newState.count++;
     this.setState(newState);
+    this.verificarVencedor();
+  }
+
+  verificarVencedor(){
+    for(const forma of this.formasDeGanho){
+      const a = this.state.values[forma[0]];
+      const b = this.state.values[forma[1]];
+      const c = this.state.values[forma[2]];
+      if(a && b && c && a === b && b === c){
+        alert('Ganhou');
+        this.setState({vencedor: true});
+        return;
+      }
+    }
   }
 
   render() {
