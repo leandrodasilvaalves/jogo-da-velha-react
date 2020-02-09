1adab274304585ec9da4e88f55d1984a5ae7b43c
Leandro da Silva Alves <leandro.alves@invillia.com>
2020-02-09 16:59:51 -0300

    Atualizar placar apos vitoria

--- src/componentes/tabuleiro/index.jsx
+++ src/componentes/tabuleiro/index.jsx
@@ -8,7 +8,7 @@ const initialState = {
   values: Array(9).fill(null),
   count: 0,
   vencedor: false,
-  players:[
+  players: [
     {
       nome: 'Jogador X',
       vitorias: 0
@@ -24,58 +24,64 @@ export default class Tabuleiro extends Component {
 
   state = {...initialState};
   formasDeGanho = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6],
-    ];
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-  clickButton(indice) {  
-    
-    if(this.state.count === 9 || this.state.values[indice] || this.state.vencedor)
+  clickButton(indice) {
+
+    if (this.state.count === 9 || this.state.values[indice] || this.state.vencedor)
       return;
 
-    let newState = {...this.state};
+    let newState = { ...this.state };
     newState.values[indice] = this.state.count % 2 === 0 ? 'X' : 'O';
     newState.count++;
     this.setState(newState);
     this.verificarVencedor();
+    
   }
 
-  verificarVencedor(){
-    for(const forma of this.formasDeGanho){
-      const[f1, f2, f3] = forma;
-      const [v1,v2,v3 ] = [this.state.values[f1], this.state.values[f2], this.state.values[f3]];
-      if(v1 && v2 && v3 && v1 === v2 && v2 === v3){
-        const indiceNome = this.state.count % 2 === 0 ? 0 : 1;
-        const {nome} = this.state.players[indiceNome];
-
-        alert(`O jogador "${nome}" venceu a partida`);
-        this.setState({vencedor: true});
+  verificarVencedor() {
+    for (const forma of this.formasDeGanho) {
+      const [f1, f2, f3] = forma;
+      const [v1, v2, v3] = [this.state.values[f1], this.state.values[f2], this.state.values[f3]];
+      if (v1 && v2 && v3 && v1 === v2 && v2 === v3) {
+        this.setState({ vencedor: true });
+        this.atualizarPlacar();
         return;
       }
     }
   }
 
+  atualizarPlacar() {
+    const indiceNome = this.state.count % 2 === 0 ? 0 : 1;
+    let jogadorVencendor = this.state.players[indiceNome];
+    jogadorVencendor.vitorias++;
+    this.setState([...this.state.players, jogadorVencendor]);
+    alert(`O jogador "${jogadorVencendor.nome}" venceu a partida`);    
+  }
+
   render() {
     const onClick = indice => this.clickButton(indice);
     const [player1, player2] = this.state.players;
-    return ( 
-      <div> 
-        <Player name={player1.nome} victories={player1.vitorias}/>
-        <Player name={player2.nome} victories={player2.vitorias}/>
+    return (
+      <div>
+        <Player name={player1.nome} victories={player1.vitorias} />
+        <Player name={player2.nome} victories={player2.vitorias} />
         <div className="tabuleiro">
           {
-            this.state.values.map((item, index) =>(
-              <Button key={index} click={onClick} indice={index} value={item}/>
+            this.state.values.map((item, index) => (
+              <Button key={index} click={onClick} indice={index} value={item} />
             ))
           }
-        </div> 
-      </div> 
+        </div>
+      </div>
     )
   }
 }
